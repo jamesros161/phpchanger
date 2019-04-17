e9de1b2b9acb6799d3e88db3e9aaef18a4b33d89
James Rosado <dev@twmsllc.com>
2019-04-17 07:42:39 -0400

    test

--- phpchanger.py
+++ phpchanger.py
@@ -61,7 +61,7 @@ def determine_uapi_access():
             ).communicate()
         data = reqOutput.splitlines()
         error = reqError.splitlines()
-        print(type(data))
+        print(type(data[0]))
         #data = json.loads(data)
         #error = json.loads(error[0])
         if args.verbose:
