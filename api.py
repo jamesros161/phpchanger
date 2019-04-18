cf5f8b4d8c85acea4d5178fe39e4a5538afeddc7
James Rosado <dev@twmsllc.com>
2019-04-17 23:59:53 -0400

    test

--- api.py
+++ api.py
@@ -69,6 +69,8 @@ class API():
         data = response['result']['data']
         users_domains = [data['main_domain']]
         users_domains = users_domains + data['sub_domains']
+        users_domains = users_domains + data['addon_domains']
+        users_domains = users_domains + data['parked_domains']
         #for value in data.iteritems:
         #    print(value)
         #users_domains = users_domains + data['main_domain']
