1ab6c9d4b9fd5369ba78d6c6a5b643db596d4821
James Rosado <dev@twmsllc.com>
2019-04-17 21:46:55 -0400

    test

--- api.py
+++ api.py
@@ -151,7 +151,7 @@ class API():
         cmd = "php_ini_get_user_content"
         domains = self.breakup_domains_by_users()
         print(domains[0]['domains'])
-        for domain in domains['domains']:        
+        for domain in domains[0]['domains']:        
             params =['type=vhost', 'vhost=' + domain]
             php_ini_settings = self.call(api, module=module, cmd=cmd, params=params)
             metadata = php_ini_settings['result']['metadata']['LangPHP']
