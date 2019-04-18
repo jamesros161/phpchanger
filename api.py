a76efe18b53f3a38c9c51a86ff6ad2c096884ffd
James Rosado <dev@twmsllc.com>
2019-04-17 23:35:41 -0400

    test

--- api.py
+++ api.py
@@ -157,9 +157,10 @@ class API():
                 while x < len(value):
                     params = ['type=vhost', 'vhost=' + value[x]]
                     php_ini_settings = self.call(api, user=user, module=module, cmd=cmd, params=params)
-                    metadata = php_ini_settings['result']['metadata']
-                    print(metadata['vhost'] + " (" + metadata['path'] + "):")
-                    print(unescape(php_ini_settings['result']['data']['content']))
+                    print(php_ini_settings)
+                    #metadata = php_ini_settings['result']['metadata']['LangPHP']
+                    #print(metadata['vhost'] + " (" + metadata['path'] + "):")
+                    #print(unescape(php_ini_settings['result']['data']['content']))
                     x += 1
             
 
