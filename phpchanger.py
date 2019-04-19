74e346d2541ff5665f6ba502e4cac36516957911
James Rosado <dev@twmsllc.com>
2019-04-18 21:24:58 -0400

    test

--- phpchanger.py
+++ phpchanger.py
@@ -16,12 +16,20 @@ def main():
     if args.verbose:
         logger.setlevel('INFO')
         logger.log('info', 'Verbose Option Enabled')
+        logger.log('warning', 'Verbose Option Enabled')
+        logger.log('error', 'Verbose Option Enabled')
+        logger.log('critical', 'Verbose Option Enabled')
     if args.debug:
         logger.setlevel('DEBUG')
         logger.log('debug', 'Debug Option Enabled')
+        logger.log('info', 'Debug Option Enabled')
+        logger.log('warning', 'Debug Option Enabled')
+        logger.log('error', 'Debug Option Enabled')
+        logger.log('critical', 'Debug Option Enabled')
     if args.quiet:
         logger.setlevel('ERROR')
         logger.log('error', 'Quit Option Enabled')
+        logger.log('critical', 'Quit Option Enabled')
     
     if hasattr(args, 'mngr_subparser'):
         if args.mngr_subparser == 'get':
